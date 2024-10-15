4c175dea6e691b6d3e6b7e85cee091cbd79f110f
himanshu9642 <165370460+himanshu9642@users.noreply.github.com>
2024-10-15 10:20:12 +0530

    C++ Program using multiple inheritance

---
+++ multiple Inheritance/multipleiheritance.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+class Person{
+    char name[30];
+    public:
+    void read(){
+        cout<<"Enter name:"<<endl;
+        cin>>name;
+    }
+    void display(){
+
+    }
+};
+class Sport{
+    int m=80;
+    public:
+    void display(){
+        cout<<m;
+    }
+};
+class Student:public Person,public Sport{
+    public:
+    void display(){
+        Person::display();
+        Sport::display();
+    }
+};
+int main(){
+    Student st;
+    st.read();
+    st.display();
+    return 0;
+}
+
